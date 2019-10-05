43013c6a8ea33005a107cb9267f796b07a866675
usmanbala12 <usman_bala12@yahoo.com>
2019-10-05 15:31:44 +0100

    added some new features

--- cal.py
+++ cal.py
@@ -1,13 +1,27 @@
 #!usr/bin/python3
 print("this program calculate simple arithmetic\nenter 'add' to add\nenter 'subs' to substract\nenter 'multiply' to multiply\nenter 'divide' to divide") 
-operation_type = str(input("enter the operation type"))
-if operation_type == "add":
+operation_type = str(input("enter the operation type: "))
+while operation_type != "add" or "subs" or "multiply" or "divide":
+    p = input("enter operation type again: ")
+    break
+    p = operation_type
+if p == "add":
     a = float(input("enter the A value"))
     b = float(input("enter the B value"))
     answer = a+b
     print("your answer is; ", answer )
-elif operation_type == "subs":
+elif p == "subs":
     c = float(input("enter the A value: "))
     d = float(input("enter the B value: "))
     answer1 = c-d
-    print("your answer is; ", answer1)    
+    print("your answer is; ", answer1)  
+elif p == "multiply":
+    e = float(input("enter the A value: "))
+    f = float(input("enter the B value: "))
+    answer2 = e * f
+    print("your result is equal: ", answer2)
+elif p == "divide":
+    g = float(input("enter the A value: "))
+    h = float(input("enter the B value: "))
+    answer3 = g / h
+    print("your result is equal to: ", answer3)  
